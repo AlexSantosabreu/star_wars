6ee17066ae2911d221708187c9b828c6ec3802fb
Alex Abreu <alexsantosdeabreu@gmail.com>
2021-09-07 08:41:19 -0300

    adding vehicle

---
+++ db/migrate/20210906010336_create_vehicles.rb
@@ -0,0 +1,19 @@
+class CreateVehicles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :vehicles do |t|
+      t.string :name
+      t.string :model
+      t.string :manufacturer
+      t.string :cost_in_credits
+      t.string :length
+      t.string :max_atmosphering_speed
+      t.string :crew
+      t.string :passengers
+      t.string :cargo_capacity
+      t.string :consumables
+      t.string :vehicle_class
+
+      t.timestamps
+    end
+  end
+end
